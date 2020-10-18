7e76eeb080935c2365fe40c30f39fa83d1d3ca5c
Benedikt Eggers <be@spiegelburg.net>
2020-10-18 14:25:28 +0200

    Add IconCache

---
+++ src/Simplic.Icon.Service/IconCache.cs
@@ -0,0 +1,32 @@
+﻿using Simplic.Cache;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.Icon.Service
+{
+    /// <summary>
+    /// Cacheable icon
+    /// </summary>
+    internal class IconCache : ICacheable
+    {
+        /// <summary>
+        /// Gets the icon cache-key
+        /// </summary>
+        public const string CacheKey = "IconCache";
+
+        public string Key => throw new NotImplementedException();
+
+        /// <summary>
+        /// Gets or sets a list of cached icons
+        /// </summary>
+        public IList<Icon> Icons { get; internal set; } = new List<Icon>();
+
+        public void OnRemove()
+        {
+
+        }
+    }
+}
