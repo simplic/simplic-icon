4b5b0e212f08f2dea045029377ff080f9ac21821
Benedikt Eggers <be@spiegelburg.net>
2020-10-18 14:55:12 +0200

    Fix NRE

--- src/Simplic.Icon.Service/IconService.cs
+++ src/Simplic.Icon.Service/IconService.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Controls;
 using Simplic.Configuration;
+using System.Security.AccessControl;
 
 namespace Simplic.Icon.Service
 {
@@ -31,10 +32,13 @@ namespace Simplic.Icon.Service
         #endregion
 
         #region Constructor
-        public IconService()
+        public IconService(ISqlService sqlService, IConfigurationService configurationService)
         {
             if (Directory.Exists(IconFolderPath) == false)
                 Directory.CreateDirectory(IconFolderPath);
+
+            this.configurationService = configurationService;
+            this.sqlService = sqlService;
         }
         #endregion
 
