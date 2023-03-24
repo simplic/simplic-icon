ea95b6e46836800ca3b431e1fcfa48778213d0f9
jason.gloger <jason.gloger@simplic.biz>
2023-03-24 10:48:12 +0100

    Added comments.

--- src/Simplic.Icon.UI/ViewModel/IsHitTestVisibleToOpacityConverter.cs
+++ src/Simplic.Icon.UI/ViewModel/IsHitTestVisibleToOpacityConverter.cs
@@ -1,24 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Data;
-
-namespace Simplic.Icon.UI
+﻿namespace Simplic.Icon.UI
 {
+    /// <summary>
+    /// Represents the class to convert a true bool value to 1 else 0.5.
+    /// </summary>
     public class IsHitTestVisibleToOpacityConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts a true bool value to 1 else 0.5.
+        /// </summary>
+        /// <param name="value">The inspected bool value.</param>
+        /// <param name="targetType">Type.</param>
+        /// <param name="parameter">object.</param>
+        /// <param name="culture">CultureInfo.</param>
+        /// <returns></returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             bool isHitTestVisible = (bool)value;
             return isHitTestVisible ? 1.0 : 0.5;
         }
 
+        /// <summary>
+        /// Converts the opacity back to true or false.
+        /// </summary>
+        /// <param name="value">The inspected double value.</param>
+        /// <param name="targetType">Type.</param>
+        /// <param name="parameter">object.</param>
+        /// <param name="culture">CultureInfo.</param>
+        /// <returns></returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            double opacity = (double)value;
+            return opacity >= 1.0 ? true : false;
         }
     }
 }
